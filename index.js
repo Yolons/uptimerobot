9a0e6e8761075efba1e6d6fc69ff0c932aa0c760
Yolons <95002845+Yolons@users.noreply.github.com>
2021-11-26 12:19:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://BOT-EXE.kenjinicholas.repl.co"]
+const urls = ["https://kanades.kenjinicholas.repl.co/"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
